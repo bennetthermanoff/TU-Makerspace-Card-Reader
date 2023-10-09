c96ec7b8e19f69be3ebb7d15e466c638df925fef
Bennett Hermanoff <bennett@hermanoff.dev>
2023-10-09 11:27:16 -0500

    disable logging

--- tu-makerspace-card-reader/server/app/models/index.js
+++ tu-makerspace-card-reader/server/app/models/index.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 require('dotenv').config();
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: './database.sqlite'
+  storage: './database.sqlite',
+  logging: false
 });
 const db = {};
 db.Sequelize = Sequelize;
